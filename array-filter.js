6360f8a781b3c554cf892e2eb1de3c42f5b4069e
biswascom <abjessbd@gmail.com>
2023-02-21 19:13:14 +0600

    array map, array filter, array find

---
+++ array-filter.js
@@ -0,0 +1,4 @@
+const dollar = [45, 4, 23, 49, 85, 09, 84];
+
+const doubleOfDollar = dollar.filter(x => x * 2 > 100);
+console.log(doubleOfDollar);
---
+++ array-map.js
@@ -0,0 +1,4 @@
+const dollar = [45, 4, 23, 49, 85, 09, 84];
+
+const doubleOfDollar = dollar.map(x => x * 2);
+console.log(doubleOfDollar);
