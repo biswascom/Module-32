0a1c5d99fc28115de93011f4172e48c22e2a54d0
biswascom <abjessbd@gmail.com>
2023-02-22 19:43:35 +0600

    class added

---
+++ class.js
@@ -0,0 +1,12 @@
+class Teachers{
+    constructor(name, subject, age, salary, posting){
+        this.name = name;
+        this.subject = subject;
+        this.age = age;
+        this.salary = salary;
+        this.posting = posting;
+    }
+};
+
+let alaminBiswas = new Teachers('Al-Amin Biswas', 'Computer Science', 32, 170000, 'Jashore');
+console.log(alaminBiswas);
