5e9b6c49bfc356aee79fb4936e64672d8512aece
biswascom <abjessbd@gmail.com>
2023-02-22 11:41:45 +0600

    ...

--- array-reduce.js
+++ array-reduce.js
@@ -1,6 +1,6 @@
 const dollar = [45, 4, 23, 49, 85, 09, 84];
 
-const sum = dollar.reduce((previous, current) =>{
+const sum = dollar.reduce((previous, current) => {
     const sum = previous + current
     return sum;
 }, 0);
